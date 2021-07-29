16c913e29a8fbaeb74c9539b834ea9afb8911316
Sujan Chhetri <44917558+sujanchhetri@users.noreply.github.com>
2021-07-29 11:47:59 +0545

    main branch path added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 const config = require('./src/config');
 
 module.exports = {
+  pathPrefix:"/thisisshivamgupta.github.io",
   siteMetadata: {
     title: config.siteTitle,
     siteUrl: config.siteUrl,
